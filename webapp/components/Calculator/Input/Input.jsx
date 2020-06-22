e9e10578e3c115d01a1554e40e1928599ca3970e
Mino Togna <mino.togna@gmail.com>
2020-06-22 18:08:29 +0200

    Input: Allowing only predefined set of char

--- webapp/components/Calculator/Input/Input.jsx
+++ webapp/components/Calculator/Input/Input.jsx
@@ -1,15 +1,56 @@
-import React, { useLayoutEffect, useRef } from 'react'
+import React, { useLayoutEffect, useRef, useState } from 'react'
 
 import style from './input.scss'
 
+const allowedChars = [
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '(',
+  ')',
+  '/',
+  '*',
+  '-',
+  '+',
+  '.',
+  ' ',
+  'ArrowLeft',
+  'ArrowRight',
+  'Backspace',
+  'Delete',
+]
+
 const Input = () => {
   const inputRef = useRef(null)
-
+  const [value, setValue] = useState('')
   useLayoutEffect(() => {
     inputRef.current.focus()
   }, [])
 
-  return <input className={style.input} type="text" ref={inputRef} />
+  return (
+    <input
+      className={style.input}
+      type="text"
+      ref={inputRef}
+      value={value}
+      onChange={(event) => {
+        setValue(event.target.value)
+      }}
+      onKeyDown={(event) => {
+        const { key } = event
+        if (allowedChars.indexOf(key) < 0) {
+          event.preventDefault()
+        }
+      }}
+    />
+  )
 }
 
 export default Input
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-// import 'dotenv/config'
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
